0ddf72c593fb4706641618fcbb05327b6d2475ea
Abhisahu2021 <118881552+Abhisahu2021@users.noreply.github.com>
2023-06-12 19:44:30 +0530

    Add files via upload

---
+++ Health_CareApplication.java
@@ -0,0 +1,16 @@
+package com.Health_Care;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class Health_CareApplication 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(Health_CareApplication.class, args);
+    }
+}
